7381f99c783d46871d273b8f0e583803c2e59709
firemax13 <33212010+firemax13@users.noreply.github.com>
2020-07-18 14:55:21 +0800

    r1q fstab

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,8 +77,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_ROOT_EXTRA_FOLDERS := \
     efs \
-    dqmdbg \
-		product
+    dqmdbg
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
